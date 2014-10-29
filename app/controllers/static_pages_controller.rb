861604612b74fd616ec0bd51f4c8bac4dd238f51
Tom Hacquoil <tom.hacquoil@calligo.net>
2014-10-29 14:47:05 +0000

    Segregated application / marketing layouts and implemented more basic styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,15 @@
 class StaticPagesController < ApplicationController
 
+
+
 	def home
+		# Use the alternative marketing layout for the front-end
+		render layout: "marketing"
 	end
 
 	def about
+		# Use the alternative marketing layout for the front-end
+		render layout: "marketing"
 	end
 
 end
