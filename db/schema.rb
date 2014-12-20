f5989f6412d196846afa94bfcfe56f6e6c698b7e
Tom Hacquoil <tom.hacquoil@calligo.net>
2014-12-20 18:49:23 +0000

    merge changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20141220183011) do
 
   create_table "applications", force: true do |t|
