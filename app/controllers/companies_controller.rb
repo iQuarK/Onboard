62c6ade10237608aa212ba79aaa20397ff852447
Tom Luce <thomasgluce@gmail.com>
2014-12-20 17:14:10 +0000

    Some stuff

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,17 +1,12 @@
 class CompaniesController < ApplicationController
 
-  before_action :authenticate_user!, except: [:show ]
-  before_action :set_company, only: [:edit, :update, :destroy]
-  before_action :check_for_existing_company, only: [:new, :create]
-
   before_action :load_company, only: [:show]
-
+  layout 'external'
 
   # -------------------------------------------------------------------------------------------------------------------
   # GET :subdomain.pinpoint.hr
   # -------------------------------------------------------------------------------------------------------------------
   def show
-    render layout: 'external'
   end
 
   # -------------------------------------------------------------------------------------------------------------------
@@ -19,14 +14,6 @@ class CompaniesController < ApplicationController
   # -------------------------------------------------------------------------------------------------------------------
   def new
     @company = Company.new
-    render layout: 'external'
-  end
-
-  # -------------------------------------------------------------------------------------------------------------------
-  # GET /companies/1/edit
-  # -------------------------------------------------------------------------------------------------------------------
-  def edit
-    render layout: 'application'
   end
 
   # -------------------------------------------------------------------------------------------------------------------
@@ -39,27 +26,8 @@ class CompaniesController < ApplicationController
       @company.company_administrators.create(user_id: current_user.id)
       redirect_to root_url(subdomain: @company.subdomain), notice: 'Company was successfully created.'
     else
-      render :new, layout: 'external'
+      render :new
     end
-  end
-
-  # -------------------------------------------------------------------------------------------------------------------
-  # PATCH/PUT /companies/1
-  # -------------------------------------------------------------------------------------------------------------------
-  def update
-    if @company.update(company_params)
-      redirect_to @company, notice: 'Company was successfully updated.'
-    else
-      render :edit, layout: 'application'
-    end
-  end
-
-  # -------------------------------------------------------------------------------------------------------------------
-  # DELETE /companies/1
-  # -------------------------------------------------------------------------------------------------------------------
-  def destroy
-    @company.destroy
-    redirect_to root_url(subdomain: false), notice: 'Company was successfully destroyed.'
   end
 
   # -------------------------------------------------------------------------------------------------------------------
@@ -67,20 +35,6 @@ class CompaniesController < ApplicationController
   # -------------------------------------------------------------------------------------------------------------------
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_company
-    @company = Company.find(params[:id])
-  end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def company_params
-    params.require(:company).permit(:name, :description, :industry, :location, :subdomain)
-  end
-
-  def check_for_existing_company
-    if current_user.has_company?
-      redirect_to dashboard_path, notice: "You already have a company"
-    end
-  end
 
 end
