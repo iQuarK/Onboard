9d381e7309071212a6ffc8911a608c522eed5886
Tom Luce <thomasgluce@gmail.com>
2014-12-21 12:42:54 +0000

    Fixed signup process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
 
   include UrlHelper
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -11,10 +13,21 @@ class ApplicationController < ActionController::Base
     dashboard_url(subdomain: false)
 	end
 
+  # Devise redirect to home page after logging out
+  def after_sign_out_path_for(resource)
+    root_url(subdomain: false)
+  end
+
   private
 
   def load_company
     @company = Company.find_by_subdomain!(request.subdomain)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:title, :first_name, :last_name, :email, :password, :password_confirmation)
+    end
   end
 
 end
