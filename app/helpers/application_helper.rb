9903b29656042d819631a18b901979847e953ba3
Tom Hacquoil <tom.hacquoil@calligo.net>
2014-12-28 14:11:40 +0000

    Added a few further LinkedIn bits in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,37 @@ module ApplicationHelper
     end
   end
 
+  # Formats LinkedIn Dates into something readable
+  def linkedin_date(month)
+  	case month
+  		when 1
+  			month = "January"
+  		when 2
+  			month = "February"
+  		when 3 
+  			month = "March"
+  		when 4
+  			month = "April"
+  		when 5
+  			month = "May"
+  		when 6
+  			month = "June"
+  		when 7
+  			month = "July"
+  		when 8
+  			month = "August"
+  		when 9
+  			month = "September"
+  		when 10
+  			month = "October"
+  		when 11
+  			month = "November"
+  		when 12
+  			month = "December"
+  		else
+  			month = ""
+  	end
+  	return month
+  end
+
 end
