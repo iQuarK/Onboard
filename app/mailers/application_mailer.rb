9a505f7cf43351a96dcee907c55cf93f25d63774
Tom Luce <thomasgluce@gmail.com>
2014-12-30 16:07:38 +0000

    Some more on emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,47 +2,27 @@ class ApplicationMailer < ActionMailer::Base
 
   def thanks(application)
     @application = application
-    mail(
-      subject: 'Application Received',
-      to: @application.email,
-      track_opens: 'true'
-    )
+    mail( subject: 'Application Received', to: @application.email, track_opens: 'true' )
   end
 
   def reject(application)
     @application = application
-    mail(
-      subject: 'Application Update',
-      to: @application.email,
-      track_opens: 'true'
-    )
+    mail( subject: 'Application Update', to: @application.email, track_opens: 'true' )
   end
 
   def interview_invite(application)
     @application = application
-    mail(
-      subject: 'Application Update',
-      to: @application.email,
-      track_opens: 'true'
-    )
+    mail( subject: 'Application Update', to: @application.email, track_opens: 'true' )
   end
 
   def info_request(application)
     @application = application
-    mail(
-      subject: 'Application Update',
-      to: @application.email,
-      track_opens: 'true'
-    )
+    mail( subject: 'Application Update', to: @application.email, track_opens: 'true' )
   end
 
   def hire(application)
     @application = application
-    mail(
-      subject: 'Application Update',
-      to: @application.email,
-      track_opens: 'true'
-    )
+    mail( subject: 'Application Update', to: @application.email, track_opens: 'true' )
   end
 
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,66 +1,44 @@
 class UserMailer < ActionMailer::Base
 
+  layout 'email_basic'
+
   def welcome(user)
     @user = user
-    mail(
-      subject: 'Welcome',
-      to: @user.email,
-      track_opens: 'true'
-    )
+
+    @title = "Welcome to Pinpoint"
+    @content =  "<p>Hi there!</p>"\
+                "<p>I'm some multi paragraph content</p>"
+    mail( subject: 'Welcome', to: @user.email, track_opens: 'true' )
   end
 
   def new_applicant(user)
     @user = user
-    mail(
-      subject: 'New Applicant',
-      to: @user.email,
-      track_opens: 'true'
-    )
+    mail( subject: 'New Applicant', to: @user.email, track_opens: 'true' )
   end
 
   def new_job(user)
     @user = user
-    mail(
-      subject: 'New Job Posting',
-      to: @user.email,
-      track_opens: 'true'
-    )
+    mail( subject: 'New Job Posting', to: @user.email, track_opens: 'true' )
   end
 
   def new_hire(user)
     @user = user
-    mail(
-      subject: 'New Hire',
-      to: @user.email,
-      track_opens: 'true'
-    )
+    mail( subject: 'New Hire', to: @user.email, track_opens: 'true' )
   end
 
   def marketing_3_day(user)
     @user = user
-    mail(
-      subject: '10 Things that you really cared about in 2014',
-      to: @user.email,
-      track_opens: 'true'
-    )
+    mail( subject: '10 Things that you really cared about in 2014', to: @user.email, track_opens: 'true' )
   end
 
   def marketing_7_day(user)
     @user = user
-    mail(
-      subject: '16 Hilarious one star reviews of recruitment agencies',
-      to: @user.email,
-      track_opens: 'true'
-    )
+    mail( subject: '16 Hilarious one star reviews of recruitment agencies', to: @user.email, track_opens: 'true' )
   end
 
   def marketing_14_day(user)
     @user = user
-    mail(
-      subject: 'The 11 most perfectly designed job postings',
-      to: @user.email,
-      track_opens: 'true'
-    )
+    mail( subject: 'The 11 most perfectly designed job postings', to: @user.email, track_opens: 'true' )
   end
 
 end
