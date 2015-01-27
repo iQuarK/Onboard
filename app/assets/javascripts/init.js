90fbb600f70b12c3ae82cffab78e14fe74a21b1a
Tom Luce <thomasgluce@gmail.com>
2015-01-27 20:51:43 +0000

    added colour thief to external company page

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -37,40 +37,6 @@ $(document).ready(function() {
   // -------------------------------------------------------------------------
   new WOW().init();
 
-  // -------------------------------------------------------------------------
-  // Colour Thief - stealing colours since 2014
-  // -------------------------------------------------------------------------
-  $('.get-colours').on('click', function(event) {
-    var colours = colorsForImage($('.company-logo'));
-    $('body').css('background-color', colours['hex']['color']);
 
-  });
-
-  var colorsForImage = function($image) {
-    var colorThief = new ColorThief();
-    var image = $image[0];
-    var color = colorThief.getColor(image);
-    var palette = colorThief.getPalette(image);
-    var colorThiefOutput = {
-      rgb: {
-        color: color,
-        palette: palette
-      },
-      hex: {
-        color: rgbToHex(color[0], color[1], color[2]),
-        palette: palette.map(function(item){ return rgbToHex(item[0], item[1], item[2]); })
-      }
-    };
-    return colorThiefOutput;
-  };
-
-  function componentToHex(c) {
-      var hex = c.toString(16);
-      return hex.length == 1 ? "0" + hex : hex;
-  }
-
-  function rgbToHex(r, g, b) {
-      return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-  }
 
 });
